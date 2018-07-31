ca65d6876abde32e07aef0dc5559d6040ca0df97
Toph <toph.matta@gmail.com>
2018-07-30 19:47:08 -0500

    Back/forward buttons index hiding

--- Ten Ideas/ListCreationViewController.swift
+++ Ten Ideas/ListCreationViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ListCreationViewController: UIViewController, UITextViewDelegate, UINavigationControllerDelegate {
+class ListCreationViewController: UIViewController, UITextViewDelegate {
     
     @IBOutlet var listNumberLabel: UILabel!
     @IBOutlet var ideaNumberLabel: UILabel!
     @IBOutlet var backButtonLabel: UIButton!
+    @IBOutlet var nextButtonLabel: UIButton!
+    @IBOutlet var finishButtonLabel: UIButton!
     @IBOutlet var contentTextView: UITextView!
     
     var placeholderLabel: UILabel!
@@ -20,38 +22,104 @@ class ListCreationViewController: UIViewController, UITextViewDelegate, UINaviga
     var currentIdea: Idea!
     
     @IBAction func nextButtonPressed(_ sender: Any) {
-        // Storing UITextView text into idea object & appending to idea list
-        currentIdea.text  = contentTextView.text
-        currentIdeaList.allIdeas.append(currentIdea)
+        appendIdea()
         pushNextViewOntoStack()
     }
     
     @IBAction func backButtonPressed(_ sender: Any) {
-        currentIdea.index -= 1
+        appendIdea()
         navigationController?.popViewController(animated: true)
     }
     
     @IBAction func finishButtonPressed(_ sender: Any) {
-        let ideaListArray = currentIdeaList.allIdeas
-        let ideaListTitle = currentIdeaList.ideaListTitle
-        
-        // Save list - UserDefaults
-        let defaults = UserDefaults.standard
-        defaults.setValue(try? PropertyListEncoder().encode(ideaListArray), forKey: ideaListTitle)
+        appendIdea()
+        encodeListAndSaveToDefaults()
+        // dimiss modal
         self.dismiss(animated: true)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        updateIndexAndRespectiveUI()
+        performMiscUIActions()
+        formatContentTextViewParameters()
+    }
+    
+    // Take content text view text and append to [Idea]
+    func appendIdea(){
+        currentIdea.text  = contentTextView.text
+        currentIdeaList.allIdeas.append(currentIdea)
+    }
+    
+    // Encode [Idea] object and save to user defaults
+    func encodeListAndSaveToDefaults(){
+        let ideaListArray = currentIdeaList.allIdeas
+        let ideaListTitle = currentIdeaList.ideaListTitle
+        
+        // Save list - UserDefaults
+        let defaults = UserDefaults.standard
+        defaults.setValue(try? PropertyListEncoder().encode(ideaListArray), forKey: ideaListTitle)
+    }
+    
+    // Update index on current view & prepare indices on proximity views
+    func updateIndexAndRespectiveUI(){
+        switch currentIdea.index {
+        case 0:
+            currentIdea.index = 1
+            currentIdea.nextIndex = currentIdea.index + 1
+            
+            // Hide back & finish button
+            backButtonLabel.isHidden = true
+            finishButtonLabel.isHidden = true
+        case 1:
+            currentIdea.index += 1
+            currentIdea.nextIndex = currentIdea.index + 1
+            currentIdea.previousIndex = nil
+            
+            // Show back button, hide finish
+            backButtonLabel.isHidden = false
+            finishButtonLabel.isHidden = true
+        case 2...8:
+            currentIdea.index += 1
+            currentIdea.nextIndex = currentIdea.index + 1
+            currentIdea.previousIndex = currentIdea.index - 1
+            
+            // Hide finish button
+            finishButtonLabel.isHidden = true
+        case 9:
+            currentIdea.index += 1
+            currentIdea.nextIndex = nil
+            currentIdea.previousIndex = currentIdea.index - 1
+            
+            // Show finish button and hide next button
+            finishButtonLabel.isHidden = false
+            nextButtonLabel.isHidden = true
+        default:
+            break
+        }
+    }
+    
+    func pushNextViewOntoStack(){
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "lvc") as! ListCreationViewController
+        destination.currentIdeaList = currentIdeaList
+        // Used struct instead of class for 'Idea' to create a copy instead of reference to be able to use below logic
+        destination.currentIdea = currentIdea
+        navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    // MARK: UI ELEMENTS
+    
+    // Miscellaneous UI actions
+    func performMiscUIActions(){
         contentTextView.delegate = self
-        listNumberLabel.text = currentIdeaList.ideaListTitle
-        
-        updateIndex()
         self.navigationItem.title = ""
-        
+        listNumberLabel.text = currentIdeaList.ideaListTitle
         ideaNumberLabel.text = String(currentIdea.index)
-        
+    }
+    
+    // Content text view functionality
+    func formatContentTextViewParameters(){
         // Logic expands UITextview as words are entered
         let fixedWidth = contentTextView.frame.size.width
         let newSize = contentTextView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
@@ -67,38 +135,7 @@ class ListCreationViewController: UIViewController, UITextViewDelegate, UINaviga
         placeholderLabel.textColor = UIColor.lightGray
         placeholderLabel.isHidden = !contentTextView.text.isEmpty
     }
-    // Update index on current view & prepare indecies on proximity views
-    func updateIndex(){
-        switch currentIdea.index {
-        case 0:
-            currentIdea.index = 1
-            currentIdea.nextIndex = currentIdea.index + 1
-        case 1:
-            currentIdea.index += 1
-            currentIdea.nextIndex = currentIdea.index + 1
-            currentIdea.previousIndex = nil
-        case 2...8:
-            currentIdea.index += 1
-            currentIdea.nextIndex = currentIdea.index + 1
-            currentIdea.previousIndex = currentIdea.index - 1
-        case 9:
-            currentIdea.index += 1
-            currentIdea.nextIndex = nil
-            currentIdea.previousIndex = currentIdea.index - 1
-        default:
-            break
-        }
-    }
     
-    func pushNextViewOntoStack(){
-        let destination = self.storyboard?.instantiateViewController(withIdentifier: "lvc") as! ListCreationViewController
-        destination.currentIdeaList = currentIdeaList
-        // Used struct instead of class for 'Idea' to create a copy instead of reference to be able to use below logic
-        destination.currentIdea = currentIdea
-        navigationController?.pushViewController(destination, animated: true)
-    }
-    
-    // MARK: UI ELEMENTS
     // Implements placeholder disappearing funcionality when begin to type
     func textViewDidChange(_ textView: UITextView) {
         placeholderLabel.isHidden = contentTextView.text.isEmpty ? false : true

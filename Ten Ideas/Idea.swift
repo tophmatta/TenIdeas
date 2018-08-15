3ede771e59fd0ce7aa49deed9560cd7e3297e0e4
tophmatta <toph.matta@gmail.com>
2018-08-14 19:29:54 -0500

    Fixed realm init issue

--- Ten Ideas/Idea.swift
+++ Ten Ideas/Idea.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 import RealmSwift
-import Realm
 
 class Idea: Object {
     @objc dynamic var text: String = ""
@@ -16,22 +15,11 @@ class Idea: Object {
     @objc dynamic var index: Int = 1
     //@objc dynamic let dateCreated: Date = Date()
     
-    required init(){
-        super.init()
-    }
-    convenience required init(text: String, bookmark: Bool, index: Int){
+    convenience  init(text: String, bookmark: Bool, index: Int){
         self.init()
         self.text = text
         self.bookmark = bookmark
         self.index = index
-    }
-    
-    required init(realm: RLMRealm, schema: RLMObjectSchema) {
-        fatalError("init(realm:schema:) has not been implemented")
-    }
-    
-    required init(value: Any, schema: RLMSchema) {
-        fatalError("init(value:schema:) has not been implemented")
     }
     
 }
